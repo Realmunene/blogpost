e721d74f2dad8cca6eddef17457e15d335ad6dee
Realmunene <josep.mwangi@student.moringaschool.com>
2022-09-02 21:29:56 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var comment = [{
+        "name":"Allan Smith","date":"10th jan, 2023","body":"I am a living testimony of the Zetu future while stating a Barber shop back in 2013"
+    }];
+    for(var i=0; i<comment.length; i++){
+        var adding = "<div class = 'commentBox'><img src='assets/images-removebg-preview.png'>"
+        $('#container').append(adding)
+    }
+})
