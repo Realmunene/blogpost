7306f38c57ec7107d40a33aebbfc1943d3cc60bc
Realmunene <josep.mwangi@student.moringaschool.com>
2022-09-04 23:10:55 +0300

    My commit

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function render (data){
-    
-}
-$(document).ready(function(){
-    var comment = [{
-        "name":"Allan Smith","date":"10th jan, 2023","body":"I am a living testimony of the Zetu future while stating a Barber shop back in 2013"
-    }];
-    for(var i=0; i<comment.length; i++){
-        var adding = "<div class = 'commentBox'><img src='assets/images-removebg-preview.png'>"
-        $('#container').append(adding)
-    }
-})
+// function render (data){
+
+// }
+// $(document).ready(function(){
+//     var comment = [{
+//         "name":"Allan Smith","date":"10th jan, 2023","body":"I am a living testimony of the Zetu future while stating a Barber shop back in 2013"
+//     }];
+//     for(var i=0; i<comment.length; i++){
+//         var adding = "<div class = 'commentBox'><img src='assets/images-removebg-preview.png'>"
+//         $('#container').append(adding)
+//     }
+// })
+const clicking = document.getElementById('btn');
